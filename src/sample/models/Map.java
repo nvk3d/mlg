93bf3c96c660273a702f7eeaa012f494db6de3f6
nvk3d <nike_bondar@mail.ru>
2017-06-18 17:32:38 +0300

    delete libgdx

--- src/sample/models/Map.java
+++ src/sample/models/Map.java
@@ -3,13 +3,12 @@ package sample.models;
 /**
  * Created by nvk3d on 17.06.17.
  */
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Map {
+
     protected String path;
 
     public Map(String path) {
         this.path = path;
     }
-
 }
